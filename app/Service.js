80b81a7bfb84d51290e92d1bae17040ba058d7ac
Manuel Morales <manuelmorales@gmail.com>
2015-11-15 17:23:02 +0000

    Added start command and output config

--- app/Service.js
+++ app/Service.js
@@ -5,6 +5,9 @@ function Service(config) {
 Service.prototype.start = function () {
 	var express = require('express');
 	var server = express();
+
+	console.log("Config: " + JSON.stringify(this.config, null, 2));
+
 	server.listen(this.config.port);
 };
 
